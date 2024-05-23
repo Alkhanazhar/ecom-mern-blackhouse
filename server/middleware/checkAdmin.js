3a77814c4a74f79761a1089dc1af82b940a30bdd
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-05-24 00:43:29 +0530

    Update checkAdmin.js

--- server/middleware/checkAdmin.js
+++ server/middleware/checkAdmin.js
@@ -3,7 +3,8 @@ import { User } from "../models/userSchema.js";
 export const checkAdmin = async (userid) => {
     const user = await User.findById(userid);
     if (user.role !== "admin" || user.role !== "Admin") {
-        return false;
+        return true;
     }
+    return false ;
   
 }
